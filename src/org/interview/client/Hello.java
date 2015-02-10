c910b5712a4fd371e163604a11dbf9251083c565
DayleChettiar <dale456@gmail.com>
2015-02-10 00:43:48 -0500

    step 2
    Change the error message when sending a name with least than 4 characters with the Send button

--- src/org/interview/client/Hello.java
+++ src/org/interview/client/Hello.java
@@ -147,7 +147,7 @@ public class Hello implements EntryPoint {
                 errorLabel.setText("");
                 String textToServer = nameField.getText();
                 if (!FieldVerifier.isValidName(textToServer)) {
-                    errorLabel.setText("Please enter more than 4 caracters");
+                    errorLabel.setText("You're name should contain more than 4 characters.");
                     return;
                 }
 
